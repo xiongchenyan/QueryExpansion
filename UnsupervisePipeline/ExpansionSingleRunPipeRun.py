1c859cb433e6287a0b410110aa444d15e09ba70a
chenyanxiong <xiongchenyan@gmail.com>
2014-03-12 14:42:44 -0400

    more config

--- UnsupervisePipeline/ExpansionSingleRunPipeRun.py
+++ UnsupervisePipeline/ExpansionSingleRunPipeRun.py
@@ -11,7 +11,7 @@ import sys
 
 
 if 2 != len(sys.argv):
-    print "1 para: conf\ncashdir\nin\nevaoutdir\nctfpath\nparaset"
+    print "1 para: conf\ncashdir\nin\nevaoutdir\nctfpath\nparaset\nqrel\nevadepth\nworig\nnumofexpterm\ndirmuprfdocnum\n"
     sys.exit()
     
     
