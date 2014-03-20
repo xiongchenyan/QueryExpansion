db8b00d038f4c5384f2e4e6f35bf52b4962609e8
chenyanxiong <xiongchenyan@gmail.com>
2014-03-20 15:07:57 -0400

    feature correlation analysis init

--- base/ExpTerm.py
+++ base/ExpTerm.py
@@ -6,6 +6,9 @@ data structure for expansion terms
 import json,math
 from json import JSONEncoder
 
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/Geektools')
+from ResultAnalysis.PearsonCoefficient import *
 
 class ExpTermC:
     
@@ -151,7 +154,34 @@ def MinMaxFeatureNormalize(lExpTerm):
             
                 
     
+def FeatureCorrelationAnalysis(lExpTerm):
+    #get all feature name
+        #record all score
+    #for each feature  get all its value's, in order, if not exist, add 0
+    #get pearson  
     
+    
+    hFeatureValue = {}
+    lScore = []
+    
+    for ExpTerm in lExpTerm:
+        lScore.append(ExpTerm.score)
+        for feature in ExpTerm.hFeature:
+            hFeatureValue[feature] = []
+    
+    for ExpTerm in lExpTerm:
+        for feature in hFeatureValue:
+            value = 0
+            if feature in ExpTerm.hFeature:
+                value = ExpTerm.hFeature[feature]
+            hFeatureValue[feature].append(value)
+    
+    hFeaturePearson = {}
+    for feature in hFeatureValue:
+        hFeaturePearson[feature] = pearson(lScore,hFeatureValue[feature])
+        
+    return hFeaturePearson
+        
     
     
     
