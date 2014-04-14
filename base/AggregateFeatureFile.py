046a0130942bf1f552639982ee00e70901d51498
chenyanxiong <xiongchenyan@gmail.com>
2014-04-14 15:25:25 -0400

    debug

--- base/AggregateFeatureFile.py
+++ base/AggregateFeatureFile.py
@@ -56,16 +56,14 @@ for line in open(sys.argv[2]):
     if not ThisExpTerm.Key() in hExpTerm:
         continue    
     if CurrentExpTerm.Key() == "":
-        CurrentExpTerm = deepcopy(hExpTerm[ThisExpTerm.Key()])
-    
+        CurrentExpTerm = deepcopy(hExpTerm[ThisExpTerm.Key()])    
     if CurrentExpTerm.Key() != ThisExpTerm.Key():
+        print "finished term [%s]" %(CurrentExpTerm.dump())
         print >>out, CurrentExpTerm.dump()
         if CurrentExpTerm.score > 0:
             lLabelCnt[1][0] += 1
         else:
-            lLabelCnt[1][1] += 1
-        
-               
+            lLabelCnt[1][1] += 1       
         CurrentExpTerm = deepcopy(hExpTerm[ThisExpTerm.Key()])
         
     
